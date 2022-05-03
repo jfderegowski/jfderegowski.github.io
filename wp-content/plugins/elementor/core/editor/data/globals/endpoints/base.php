00abdb15caa6a8675c1d96e15c90d67e2f86bd29
Janek <jasiojfd@gmail.com>
2022-05-03 04:47:16 +0200

    1

--- wp-content/plugins/elementor/core/editor/data/globals/endpoints/base.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Class &quot;Elementor\Data\V2\Base\Endpoint&quot; not found in C:\xampp\htdocs\MyWebsite\wp-content\plugins\elementor\core\editor\data\globals\endpoints\base.php:9
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\MyWebsite\wp-content\plugins\elementor\core\editor\data\globals\endpoints\base.php</b> on line <b>9</b><br>
